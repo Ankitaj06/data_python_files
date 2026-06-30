5bb33e265563f0d78e5a5b87c7d516d83cac13d2
Ankita <ankitaj062004@gmail.com>
2026-06-30 21:24:49 +0530

    variables part of python

--- python2.py
+++ python2.py
@@ -1,3 +1,8 @@
-print("Hello python ")
+name = "ankita" #str
+age = 20 #int
+marks = 80.86 #float
+is_pass = True #boolean
 
-print("just pushing another file")
+print(type(name),type(age),type(marks),type(is_pass))
+
+
