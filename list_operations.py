4db450ce8d8113e88996dd965ffb1e726e913567
Ankita <ankitaj062004@gmail.com>
2026-06-30 21:34:47 +0530

    added new file

---
+++ list_operations.py
@@ -0,0 +1,13 @@
+skills = ["python","pandas","numpy","ml"]
+print(skills)
+
+skills.append("sql")
+print(skills)
+skills.remove("ml")
+print(skills)
+
+print(skills[0])
+print(skills[-1])
+
+print(skills[1:2])
+print(skills[1:3])
